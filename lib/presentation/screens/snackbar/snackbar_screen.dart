de03d0f7cb77eef6322c61f8bac8f71c59ff1420
KeingCasabona <keing.limache.casabona@gmail.com>
2024-12-09 21:48:45 -0500

    feat: Add SnackBar

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -5,8 +5,31 @@ class SnackbarScreen extends StatelessWidget {
 
   static const String name = 'snackbar_screen';
 
+  void showCustomSnackbar(BuildContext context) {
+    //CON ESTO CIERRO EL SNACKBAR PARA CUANDO PRESIONE VARIAS VECES NO PAREZCA QUE SE COLGO:
+    ScaffoldMessenger.of(context).clearSnackBars();
+
+    final snackback = SnackBar(
+      content: Text('Hola Mundo'),
+      action: SnackBarAction(label: 'Ok', onPressed: () {}),
+      duration: Duration(milliseconds: 200),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackback);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Snackbar y Dialogos'),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        label: Text('Mostrar Snackbar'),
+        icon: Icon(Icons.remove_red_eye),
+        onPressed: () {
+          showCustomSnackbar(context);
+        },
+      ),
+    );
   }
 }
