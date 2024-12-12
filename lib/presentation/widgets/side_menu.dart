8fa5ad196b1fc4228fa71237d7459419af47c8c7
KeingCasabona <keing.limache.casabona@gmail.com>
2024-12-11 19:27:31 -0500

    feat: Add NavigationDrawer

---
+++ lib/presentation/widgets/side_menu.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SideMenu extends StatefulWidget {
+  const SideMenu({super.key});
+
+  @override
+  State<SideMenu> createState() => _SideMenuState();
+}
+
+class _SideMenuState extends State<SideMenu> {
+  int navDrawerIndex = 1;
+  @override
+  Widget build(BuildContext context) {
+    return NavigationDrawer(
+      selectedIndex: navDrawerIndex,
+      onDestinationSelected: (value) {
+        setState(() {
+          navDrawerIndex = value;
+        });
+      },
+      children: [
+        NavigationDrawerDestination(
+          icon: Icon(Icons.add),
+          label: Text('Home Screen'),
+        ),
+        NavigationDrawerDestination(
+            icon: Icon(Icons.card_travel_outlined),
+            label: Text('Otra pantalla'))
+      ],
+    );
+  }
+}
